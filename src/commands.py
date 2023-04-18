0515b8e59795a2b36d50507e6243f899d3e5ca8e
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-18 21:11:18 +0200

    Update commands.py
    Set as well documented module.

--- src/commands.py
+++ src/commands.py
@@ -4,8 +4,18 @@ commands.py
 A module to evaluate code and return the result.
 """
 
+import sys
+
 def evaluate_code(code):
-	"""Evaluates the code and returns the result"""
+	"""
+    Evaluates the code and returns the result.
+
+    Args:
+        code (str): The code to be evaluated.
+
+    Returns:
+        The result of the evaluation or an error message if an exception is raised.
+    """
 	result = None
 	try:
 		result = eval(code)
@@ -20,3 +30,11 @@ def evaluate_code(code):
 		return f'Error: {zde}'
 	except Exception as e:
 		return f'Unexpected error: {e}'
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        code = sys.argv[1]
+        print(evaluate_code(code))
+    else:
+        print("Please provide code to evaluate as an argument.")
