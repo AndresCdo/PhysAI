cd35fccdee6d59fe40f97016ef2e6044d3607baf
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-18 21:10:21 +0200

    Update commands.py
    Create more exceptions in evaluate_code function.

--- src/commands.py
+++ src/commands.py
@@ -7,8 +7,16 @@ A module to evaluate code and return the result.
 def evaluate_code(code):
 	"""Evaluates the code and returns the result"""
 	result = None
-	try: # Try to evaluate the code
+	try:
 		result = eval(code)
-		print(f'Result: {result}') # Print the result
-	except Exception as e: # If there is an error, print it
-		print(f'Error: {e}')
+		return f'Result: {result}'
+	except SyntaxError as se:
+		return f'Error: {se}'
+	except NameError as ne:
+		return f'Error: {ne}'
+	except TypeError as te:
+		return f'Error: {te}'
+	except ZeroDivisionError as zde:
+		return f'Error: {zde}'
+	except Exception as e:
+		return f'Unexpected error: {e}'
