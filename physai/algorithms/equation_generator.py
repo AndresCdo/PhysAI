7a9a9a79148fd58509d820168fc578c29a0b49d5
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-19 20:51:54 +0200

    Update equation_generator.py

--- physai/algorithms/equation_generator.py
+++ physai/algorithms/equation_generator.py
@@ -1,9 +1,10 @@
 import numpy as np
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 class EquationGenerator:
     """A class to generate physical equations using machine learning algorithms."""
 
-    def __init__(self, model, data):
+    def __init__(self, data, model_name="gpt2"):
         """
         Initialize the EquationGenerator with a machine learning model and training data.
 
@@ -11,8 +12,9 @@ class EquationGenerator:
             model: A machine learning model for generating physical equations.
             data: Preprocessed training data.
         """
-        self.model = model
-        self.data = data
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name) # Load the tokenizer for the model
+        self.model = GPT2LMHeadModel.from_pretrained(model_name) # Load the model itself
+        self.data = data # Store the training data for later use
 
     def train(self, epochs, batch_size):
         """
@@ -26,7 +28,7 @@ class EquationGenerator:
  
         self.model.fit(self.data, epochs=epochs, batch_size=batch_size)
 
-    def generate_equation(self, input_data):
+    def generate_equation(self, input_text, max_length=50, num_return_sequences=1):
         """
         Generate a physical equation using the trained machine learning model.
 
@@ -37,9 +39,30 @@ class EquationGenerator:
             equation: A string representation of the generated equation.
         """
         # Implement the equation generation logic for your specific model here.
+        input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
 
-        equation = self.model.predict(input_data)
-        return equation
+                # Generate output sequences
+        output_sequences = self.model.generate(
+            input_ids=input_ids,
+            max_length=max_length,
+            num_return_sequences=num_return_sequences,
+            no_repeat_ngram_size=2,
+            temperature=0.7,
+            top_k=50,
+            top_p=0.95,
+            do_sample=True,
+        )
+
+        # Decode and return the generated sequences
+        generated_equations = []
+        for sequence in output_sequences:
+            decoded_sequence = self.tokenizer.decode(sequence, skip_special_tokens=True)
+            generated_equations.append(decoded_sequence)
+
+        # Generate the equation
+        equation = self.model.predict(generated_equations)
+
+        return generated_equations
 
     def save_model(self, file_path):
         """
