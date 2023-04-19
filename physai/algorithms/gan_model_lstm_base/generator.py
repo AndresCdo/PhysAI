fd5cdc749f3c14efc7028bb3cecba9e9372b71bc
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-19 20:52:01 +0200

    Create generator.py

---
+++ physai/algorithms/gan_model_lstm_base/generator.py
@@ -0,0 +1,112 @@
+import numpy as np
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import LSTM, Dense, Embedding, Input
+from tensorflow.keras.optimizers import Adam
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+class GANModel:
+    def __init__(self, data, model_name="gpt2"):
+        """ Initialize the GANModel with a machine learning model and training data."""
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+        self.model = GPT2LMHeadModel.from_pretrained(model_name)
+        self.data = data
+
+    def build_model(self, max_length, vocab_size):
+        """ Build the GAN model."""
+        generator = Sequential([
+            Embedding(vocab_size, 128, input_length=max_length),
+            LSTM(256, return_sequences=True),
+            Dense(vocab_size, activation="softmax")
+        ])
+
+        discriminator = Sequential([
+            Embedding(vocab_size, 128, input_length=max_length),
+            LSTM(256),
+            Dense(1, activation="sigmoid")
+        ])
+        discriminator.compile(loss="binary_crossentropy", optimizer=Adam(0.0002, 0.5), metrics=["accuracy"])
+
+        discriminator.trainable = False
+        gan_input = Input(shape=(max_length,))
+        generated_sequence = generator(gan_input)
+        gan_output = discriminator(generated_sequence)
+
+        gan = Model(gan_input, gan_output)
+        gan.compile(loss="binary_crossentropy", optimizer=Adam(0.0002, 0.5))
+
+        return gan, generator, discriminator
+
+    def train(self, input_sequences, epochs, batch_size, max_length):
+        """ Train the GAN model."""
+        gan, generator, discriminator = self.build_model(max_length, len(self.tokenizer))
+
+        for epoch in range(epochs):
+            real_indices = np.random.randint(0, input_sequences.shape[0], batch_size)
+            real_samples = input_sequences[real_indices]
+
+            noise = np.random.normal(0, 1, (batch_size, max_length))
+            fake_samples = generator.predict(noise)
+
+            combined_samples = np.concatenate((real_samples, fake_samples))
+            labels = np.concatenate((np.ones((batch_size, 1)), np.zeros((batch_size, 1))))
+
+            discriminator_loss = discriminator.train_on_batch(combined_samples, labels)
+
+            generator_labels = np.ones((batch_size, 1))
+            generator_loss = gan.train_on_batch(noise, generator_labels)
+
+            print(f"Epoch {epoch}: Generator loss: {generator_loss}, discriminator loss: {discriminator_loss}")
+
+        generator.save("generator.h5")
+
+    def generate_equation_from_trained_gan(self, max_length):
+        """ Generate an equation from a trained GAN model."""
+        generator = load_model("generator.h5")
+        noise = np.random.normal(0, 1, (1, max_length))
+        generated_tokens = generator.predict(noise)[0]
+
+        generated_equation = ""
+        for token in generated_tokens:
+            if token == 0:
+                break
+            generated_equation += self.tokenizer.index_word[token] + " "
+    
+        return generated_equation
+    
+    def generate_equation_from_trained_gan_with_input(self, input_text, max_length):
+        """ Generate an equation from a trained GAN model with the given input text."""
+        generator = load_model("generator.h5")
+        input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
+        input_tokens = input_ids[0].numpy()
+        input_tokens = np.pad(input_tokens, (0, max_length - len(input_tokens)), "constant", constant_values=0)
+        input_tokens = input_tokens.reshape(1, max_length)
+        generated_tokens = generator.predict(input_tokens)[0]
+
+        generated_equation = ""
+        for token in generated_tokens:
+            if token == 0:
+                break
+            generated_equation += self.tokenizer.index_word[token] + " "
+    
+        return generated_equation
+    
+    def generate_equation_from_trained_gan_with_input_and_noise(self, input_text, max_length):
+        """ Generate an equation from a trained GAN model with the given input text and noise."""
+        generator = load_model("generator.h5")
+        input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
+        input_tokens = input_ids[0].numpy()
+        input_tokens = np.pad(input_tokens, (0, max_length - len(input_tokens)), "constant", constant_values=0)
+        input_tokens = input_tokens.reshape(1, max_length)
+        generated_tokens = generator.predict(input_tokens)[0]
+
+        noise = np.random.normal(0, 1, (1, max_length))
+        generated_tokens = generated_tokens + noise[0]
+        generated_tokens = generated_tokens.reshape(1, max_length)
+
+        generated_equation = ""
+        for token in generated_tokens:
+            if token == 0:
+                break
+            generated_equation += self.tokenizer.index_word[token] + " "
+    
+        return generated_equation
