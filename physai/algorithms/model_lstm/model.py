a6167567ce981ac0d27a5a88cea1d8e31b91f3da
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-04-24 10:55:49 +0200

    Update model.py

--- physai/algorithms/model_lstm/model.py
+++ physai/algorithms/model_lstm/model.py
@@ -1,38 +1,47 @@
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Embedding
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-# Preprocess LaTeX data (assuming you have a list of LaTeX-formatted strings)
-latex_data = ["..."]
+class LaTeXModel:
+    def __init__(self, latex_data, epochs=50, batch_size=64):
+        self.latex_data = latex_data
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.tokenizer = Tokenizer(char_level=True)
+        self.tokenizer.fit_on_texts(latex_data)
+        self.vocab_size = len(self.tokenizer.word_index) + 1
+        self.max_length = None
+        self.input_sequences, self.output_sequences = self.prepare_sequences()
+        self.model = self.build_model()
 
-# Tokenize the text
-tokenizer = Tokenizer(char_level=True)
-tokenizer.fit_on_texts(latex_data)
-vocab_size = len(tokenizer.word_index) + 1
+    def prepare_sequences(self):
+        sequences = self.tokenizer.texts_to_sequences(self.latex_data)
+        input_sequences, output_sequences = [], []
 
-# Convert text to sequences
-sequences = tokenizer.texts_to_sequences(latex_data)
+        for sequence in sequences:
+            for i in range(1, len(sequence)):
+                input_sequences.append(sequence[:i])
+                output_sequences.append(sequence[i])
 
-# Prepare input-output pairs
-input_sequences, output_sequences = [], []
-for sequence in sequences:
-    for i in range(1, len(sequence)):
-        input_sequences.append(sequence[:i])
-        output_sequences.append(sequence[i])
+        max_length = max([len(seq) for seq in input_sequences])
+        self.max_length = max_length
+        input_sequences = pad_sequences(input_sequences, maxlen=max_length, padding="pre")
+        output_sequences = np.array(output_sequences)
 
-# Pad sequences
-max_length = max([len(seq) for seq in input_sequences])
-input_sequences = pad_sequences(input_sequences, maxlen=max_length, padding="pre")
-output_sequences = np.array(output_sequences)
+        return input_sequences, output_sequences
 
-# Build the LSTM model
-model = Sequential()
-model.add(Embedding(vocab_size, 128, input_length=max_length))
-model.add(LSTM(256))
-model.add(Dense(vocab_size, activation="softmax"))
-model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    def build_model(self):
+        model = Sequential([
+            Embedding(self.vocab_size, 128, input_length=self.max_length),
+            LSTM(256),
+            Dense(self.vocab_size, activation="softmax")
+        ])
+        model.compile(loss="sparse_categorical_crossentropy", optimizer=Adam(), metrics=["accuracy"])
 
-# Train the LSTM model
-model.fit(input_sequences, output_sequences, epochs=50, batch_size=64)
+        return model
+
+    def train(self):
+        self.model.fit(self.input_sequences, self.output_sequences, epochs=self.epochs, batch_size=self.batch_size)
